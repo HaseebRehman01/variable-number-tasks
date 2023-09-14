ac9c48cc9d614a2a1210fd8c0e05a8dec9ca2631
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2023-09-15 01:48:14 +0500

    updated

--- app.js
+++ app.js
@@ -32,12 +32,14 @@
 // var variable1, variable2, variable3;
 
 
-var $name ="$name"
-var _name ="_name"
-var name ="name"
+// var $name ="$name"
+// var _name ="_name"
+// var name ="name"
 
-var $my_1stVariable = "$my_1stVariable"
-document.write("Variable names can only contain underscore,number and $.for Example: "+ $my_1stVariable +"<br>")
-document.write("Variables must begin with a $ , letter or _ . For example " +$name+ " ,  " +_name +" or " +name +"<br>")
-document.write("Variable names are case sensitive"+" <br>")
-document.write("Variable names should not be JS ")
+// var $my_1stVariable = "$my_1stVariable"
+// document.write("Variable names can only contain underscore,number and $.for Example: "+ $my_1stVariable +"<br>")
+// document.write("Variables must begin with a $ , letter or _ . For example " +$name+ " ,  " +_name +" or " +name +"<br>")
+// document.write("Variable names are case sensitive"+" <br>")
+// document.write("Variable names should not be JS ")
+
+// complete
