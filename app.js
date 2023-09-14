3024a3bf06ab4d3164568bd7b3514277b8a191ab
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2023-09-15 00:13:57 +0500

    updated

--- app.js
+++ app.js
@@ -16,10 +16,28 @@
 
 
 // task 4
-var name = "john doe"
-var total = 5
-var tShirts ="5 T-shirts"
-document.write(name+" ordered "+tShirts+" (s) on XYZ clothing store")
+
+// var name = "john doe"
+// var total = 5
+// var tShirts ="5 T-shirts"
+// document.write(name+" ordered "+tShirts+" (s) on XYZ clothing store")
+
+// complete
 
 
+// legal ,illegal variable 
 
+
+// task 1
+// var variable1, variable2, variable3;
+
+
+var $name ="$name"
+var _name ="_name"
+var name ="name"
+
+var $my_1stVariable = "$my_1stVariable"
+document.write("Variable names can only contain underscore,number and $.for Example: "+ $my_1stVariable +"<br>")
+document.write("Variables must begin with a $ , letter or _ . For example " +$name+ " ,  " +_name +" or " +name +"<br>")
+document.write("Variable names are case sensitive"+" <br>")
+document.write("Variable names should not be JS ")
