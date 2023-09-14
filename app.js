a0a7818006fc2ae651fbe2b3961b019af4f23d29
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2023-09-14 23:22:53 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// task 1
+
+// var age = 18;
+// alert("I am " + age + " years old" )
+
+// task 2
+
+// var times = 15;
+// alert ("Ahmed Saleem you have visited this site for "
+// + times+" times") 
+
+// task 3
+// var birthAge = 2005
+// document.write (" My date of birth is "+birthAge+"<br>" +" i wrote my birth date in  data type number" )
+
+
+
+// task 4
+var name = "john doe"
+var total = 5
+var tShirts ="5 T-shirts"
+document.write(name+" ordered "+tShirts+" (s) on XYZ clothing store")
+
+
+
